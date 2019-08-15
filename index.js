3bfa58a1e0404b4d444738953b17ea14ddc9de82
Ahmad Awais <me@AhmadAwais.com>
2019-08-15 14:13:28 +0500

    📖 DOC: Function

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 /* RESIZE OPTIMIZE IMAGES */
+/**
+ * Resize + optimize images.
+ *
+ * @param Array images Array of images paths.
+ * @param Number width A number value of width e.g. 1920.
+ * @param Number height Optional number value of height e.g. 1080.
+ * @param Number height Optional number value of quality of the image e.g. 90.
+ */
 
 module.exports = () => {};
